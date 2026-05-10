5a1a24a4d62bd9c67cbb291c5ff5b914c7982f43
Sonal Tamse <sonal.tamse@gmail.com>
2026-05-10 14:19:58 +0530

    Initial Spring Security project setup with stateless Basic Authentication configuration

---
+++ src/main/java/com/example/identity_access_management/IdentityAccessManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.identity_access_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAccessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAccessManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/identity_access_management/IdentityAccessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.identity_access_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAccessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
